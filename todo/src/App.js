1ce98e393103e00d7df3b7062b409ab01b208d0e
TomaszSzydlowski <TSzydlowski@protonmail.com>
2022-05-12 16:03:33 +0200

    my first commit

--- todo/src/App.js
+++ todo/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Czesc Wojtek, rozpoczyna sie Twoja przygoda z reactem. Have fun.
         </p>
         <a
           className="App-link"
