988ab0335496ad9a5db0582b2d03e9e0c4cdec2c
TomaszSzydlowski <TSzydlowski@protonmail.com>
2022-05-26 16:54:47 +0200

    add more logic add todos

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,51 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+import {
+  EditOutlined,
+  DeleteOutlined
+} from '@ant-design/icons';
+
 export const App = () => {
   const [todos, setTodos] = useState([{
-    text: "Wstac z lozka",
+    id: new Date().getTime(),
+    title: "Obiad",
+    descrption: "Lasagne i zupa ogórkowa",
     isDone: false
   }])
 
-  const addTodo2 = (text) =>{
-    const newTodos = [...todos, {text}];
+  const addTodo = (todo) => {
+    const newTodos = [...todos, { id: new Date().getTime(), title: todo.title, descrption: todo.descrption, isDone: false }];
     setTodos(newTodos);
   }
 
-  function addTodo(text){
-    const newTodos = [...todos, {text}];
-    setTodos(newTodos);
+  const markDone = (e) => {
+    const newTodos = todos.map((todo) => ({
+      ...todo,
+      isDone: todo.id.toString() === e.target.id.toString() ? !todo.isDone : todo.isDone
+    }))
+    setTodos(newTodos)
   }
 
   return (
     <div className='app'>
       <div className='container'>
-        <h1 className='text-center mb-4'>Todo List</h1>
         <FormTodo addTodo={addTodo} />
-        {todos.map((x)=>(
-          <div>{x.text}</div>
-          ))}
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <div>
+              <input type="checkbox" id={todo.id} key={todo.id} onClick={markDone} />
+            </div>
+            <div>
+              <h4 className={todo.isDone ? 'line-through' : ''}>{todo.title}</h4>
+              <p className={todo.isDone ? 'line-through' : ''}>{todo.descrption}</p>
+            </div>
+            <div><EditOutlined /><DeleteOutlined /></div>
+          </div>
+        ))}
       </div>
     </div>
   )
 }
 
 export const FormTodo = ({ addTodo }) => {
-  const [value, setValue] = useState("")
+  const [title, setTitle] = useState("")
+  const [descrption, setDescription] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if (!value) return
-    addTodo(value);
-    setValue("");
+    if (!title || !descrption) return
+    addTodo({ title: title, descrption: descrption });
+    setTitle("");
+    setDescription("");
   }
 
   return (
     <div>
-      <h3>Add Todo</h3>
-      <input type="text" value={value} onChange={e => setValue(e.target.value)}></input>
-      <button onClick={handleSubmit}>Submit</button>
+      <h3>MOJE ZADANIA</h3>
+      <input placeholder='Tytuł' type="text" value={title} onChange={e => setTitle(e.target.value)}></input>
+      <input placeholder='Opis...' type="text" value={descrption} onChange={e => setDescription(e.target.value)}></input>
+      <button onClick={handleSubmit}>Dodaj</button>
     </div>
   )
 }
